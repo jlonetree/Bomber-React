bfd588657039707b2a5976cdeb1a2ee65203d20a
IainLR <iainrobertzon@gmail.com>
2020-11-13 09:23:09 -0600

    batty

--- src/config/store.js
+++ src/config/store.js
@@ -2,10 +2,26 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { createStore } from 'redux'
 import playerReducer from '../features/player/reducer'
 import mapReducer from '../features/map/reducer'
+//
+// const decrement = () => {
+//     return{
+//         type: 'DECREMENT'
+//     }
+// }
 
+const bombReducer = (state = 7, action) => {
+    switch(action.type) {
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+//
 const rootReducer = combineReducers({
     player: playerReducer,
-    map: mapReducer
+    map: mapReducer,
+    bomb: bombReducer
 })
 
 const store = createStore(
